1458f67fb794ef21392a1fa63c6ab14851a9fb23
adamochi <honeyrumballs@gmail.com>
2022-05-26 13:56:26 +1000

    sum of all positive numbers in an array

---
+++ js/add_only_positive.js
@@ -0,0 +1,8 @@
+function positiveSum(arr) {
+  console.log(arr);
+  arr = arr.filter((arr) => arr > 0);
+  console.log(arr);
+  console.log(arr.reduce((a, b) => a + b, 0));
+}
+
+positiveSum([33, 52, 23, 3, -22, -5324]);
