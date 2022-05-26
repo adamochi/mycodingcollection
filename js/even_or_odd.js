32e9a3239814b29b61b36bb37435f91290467b96
adamochi <honeyrumballs@gmail.com>
2022-05-26 13:24:52 +1000

    Code Wars - my coding challenge collection

---
+++ js/even_or_odd.js
@@ -0,0 +1,10 @@
+function even_or_odd(number) {
+  if (number % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(even_or_odd(5));
+console.log(even_or_odd(3456));
+console.log(even_or_odd(2345));
